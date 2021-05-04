67d5aed42541afcd5b2d79d419fee6b86855f533
Vasil Dimov <vasil.dimov91@gmail.com>
2021-05-04 17:14:56 +0300

    Fix validation problems with tinyMCE

--- Web/ForumSystem.Web.ViewModels/Posts/PostInputModel.cs
+++ Web/ForumSystem.Web.ViewModels/Posts/PostInputModel.cs
@@ -9,6 +9,7 @@
         public string Title { get; set; }
 
         [Required]
+        [MinLength(100, ErrorMessage ="Content is way to short. Tell me more.")]
         public string Content { get; set; }
 
         public int CategoryId { get; set; }
