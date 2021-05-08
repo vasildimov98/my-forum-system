c3e8e264ded39a9c9a67afe34877cc8129353cfd
Vasil Dimov <vasil.dimov91@gmail.com>
2021-05-08 18:47:42 +0300

    Change Home page layout

--- Web/ForumSystem.Web.ViewModels/Home/HomePostViewModel.cs
+++ Web/ForumSystem.Web.ViewModels/Home/HomePostViewModel.cs
@@ -1,19 +1,25 @@
 ﻿namespace ForumSystem.Web.ViewModels.Home
 {
     using System;
+    using System.Linq;
     using System.Net;
     using System.Text.RegularExpressions;
 
+    using AutoMapper;
+
     using ForumSystem.Data.Models;
+    using ForumSystem.Data.Models.Enums;
     using ForumSystem.Services.Mapping;
 
-    public class HomePostViewModel : IMapFrom<Post>
+    public class HomePostViewModel : IMapFrom<Post>, IHaveCustomMappings
     {
         public int Id { get; set; }
 
         public string Title { get; set; }
 
         public string Content { get; set; }
+
+        public int CommentsCount { get; set; }
 
         public string ShortContent
         {
@@ -29,5 +35,30 @@
         public DateTime CreatedOn { get; set; }
 
         public string CategoryImage { get; set; }
+
+        public string UserUserName { get; set; }
+
+        public int Likes { get; set; }
+
+        public int Dislikes { get; set; }
+
+        public void CreateMappings(IProfileExpression configuration)
+        {
+            configuration
+                .CreateMap<Post, HomePostViewModel>()
+                .ForMember(x => x.Likes, opt =>
+                {
+                    opt.MapFrom(y => y.Votes
+                        .Count());
+                });
+
+            configuration
+                .CreateMap<Post, HomePostViewModel>()
+                .ForMember(x => x.Dislikes, opt =>
+                {
+                    opt.MapFrom(y => y.Votes
+                        .Count());
+                });
+        }
     }
 }
